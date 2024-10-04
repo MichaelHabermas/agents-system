d8cc4e14abf44933a2c2b562d782c77d3708c278
habermoose <habermoose@gmail.com>
2024-10-04 17:26:33 -0400

    default

--- app/agent_supervisor.py
+++ app/agent_supervisor.py
@@ -44,6 +44,7 @@ function_def = {
             "next": {
                 "title": "Next",
                 "anyOf": [{"enum": options}],
+                "default": "Supervisor"
             }
         },
         "required": ["next"],
